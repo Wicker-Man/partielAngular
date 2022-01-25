57575f4a31520dc338355837117cf598556a0be6
Wicker-Man <jeremie.malezieux@gmail.com>
2022-01-25 11:45:58 +0100

    Exercice5

--- partielAngular/src/app/app.component.ts
+++ partielAngular/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'partielAngular';
+}
+
+export class StatistiqueComponent {
+  pays: string;
+  nouveauxCas: number
+
+  constructor() {
+    this.pays = "France";
+    this.nouveauxCas = 353648;
+  }
+
 }
